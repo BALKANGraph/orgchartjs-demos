9004416cd1aeafdcc2314fe837e1764ebfce47c7
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-01-21 12:12:13 +0200

    auto

--- ExpanCollapseToLevel/demo.js
+++ ExpanCollapseToLevel/demo.js
@@ -1,43 +1,31 @@
 
 
 window.onload = function () {   
-    var chart = new OrgChart(document.getElementById("tree"), {
-        mouseScrool: OrgChart.action.none,
-        		nodeMenu:{
-            	edit: {text:"Edit"},
-            	add: {text:"Add"},
-            	remove: {text:"Remove"}
-            },
-            
-            nodeBinding: {
-                field_0: "id"
-            },
-            
-        });  
-        
-        nodes = [
-                { id: 1 },
-                { id: 2, pid: 1 },
-                { id: 3, pid: 1 },
-                { id: 4, pid: 2 },
-                { id: 5, pid: 2 },
-                { id: 6, pid: 3 },
-                { id: 7, pid: 3 },
-                { id: 8, pid: 4 },
-                { id: 9, pid: 4 },
-                { id: 10, pid: 5 },
-                { id: 11, pid: 5 },
-                { id: 12, pid: 6 },
-            
-            ];
+    var chart = new OrgChart(document.getElementById("tree"), {   
+        enableSearch: false,
+        collapse: {
+            level: 3
+        },
+        nodeBinding: {
+            field_0: "id",
+            field_1: "pid",
+        }
+    });    
 
-        
-        chart.load(nodes);
-        chart.collapse(3, [6, 7], function () { 
-            chart.collapse(5, [10, 11]); 
+    chart.load([
+            { id: 1},
+            { id: 2, pid: 1},
+            { id: 3, pid: 1},
+            { id: 4, pid: 2},
+            { id: 5, pid: 2},
+            { id: 6, pid: 3},
+            { id: 7, pid: 3},
+            { id: 8, pid: 4},
+        ]);
+    
+    document.getElementById("levelCount").addEventListener("change", function(e) {          
+      chart.expandCollapseToLevel(1, {
+            level: e.target.value
         });
-        
-        document.querySelector('#btn').addEventListener('click', function(){
-    	    chart.expand(null, "all");
-        });
+    });
 };
--- ExpandAll/demo.js
+++ ExpandAll/demo.js
@@ -1,31 +1,43 @@
 
 
 window.onload = function () {   
-    var chart = new OrgChart(document.getElementById("tree"), {   
-        enableSearch: false,
-        collapse: {
-            level: 3
-        },
-        nodeBinding: {
-            field_0: "id",
-            field_1: "pid",
-        }
-    });    
+    var chart = new OrgChart(document.getElementById("tree"), {
+        mouseScrool: OrgChart.action.none,
+        		nodeMenu:{
+            	edit: {text:"Edit"},
+            	add: {text:"Add"},
+            	remove: {text:"Remove"}
+            },
+            
+            nodeBinding: {
+                field_0: "id"
+            },
+            
+        });  
+        
+        nodes = [
+                { id: 1 },
+                { id: 2, pid: 1 },
+                { id: 3, pid: 1 },
+                { id: 4, pid: 2 },
+                { id: 5, pid: 2 },
+                { id: 6, pid: 3 },
+                { id: 7, pid: 3 },
+                { id: 8, pid: 4 },
+                { id: 9, pid: 4 },
+                { id: 10, pid: 5 },
+                { id: 11, pid: 5 },
+                { id: 12, pid: 6 },
+            
+            ];
 
-    chart.load([
-            { id: 1},
-            { id: 2, pid: 1},
-            { id: 3, pid: 1},
-            { id: 4, pid: 2},
-            { id: 5, pid: 2},
-            { id: 6, pid: 3},
-            { id: 7, pid: 3},
-            { id: 8, pid: 4},
-        ]);
-    
-    document.getElementById("levelCount").addEventListener("change", function(e) {          
-      chart.expandCollapseToLevel(1, {
-            level: e.target.value
+        
+        chart.load(nodes);
+        chart.collapse(3, [6, 7], function () { 
+            chart.collapse(5, [10, 11]); 
         });
-    });
+        
+        document.querySelector('#btn').addEventListener('click', function(){
+    	    chart.expand(null, "all");
+        });
 };
