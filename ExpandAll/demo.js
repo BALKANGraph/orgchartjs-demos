9c6e4de925711b9a49afa1b43701b10575b1ff14
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-01-23 13:00:32 +0200

    auto

--- ExpandAll/demo.js
+++ ExpandAll/demo.js
@@ -1,5 +1,3 @@
-
-
 window.onload = function () {   
     var chart = new OrgChart(document.getElementById("tree"), {
         mouseScrool: OrgChart.action.none,
@@ -8,13 +6,13 @@ window.onload = function () {
             	add: {text:"Add"},
             	remove: {text:"Remove"}
             },
-            
+
             nodeBinding: {
                 field_0: "id"
             },
-            
+
         });  
-        
+
         nodes = [
                 { id: 1 },
                 { id: 2, pid: 1 },
@@ -28,15 +26,15 @@ window.onload = function () {
                 { id: 10, pid: 5 },
                 { id: 11, pid: 5 },
                 { id: 12, pid: 6 },
-            
+
             ];
 
-        
+
         chart.load(nodes);
         chart.collapse(3, [6, 7], function () { 
             chart.collapse(5, [10, 11]); 
         });
-        
+
         document.querySelector('#btn').addEventListener('click', function(){
     	    chart.expand(null, "all");
         });
