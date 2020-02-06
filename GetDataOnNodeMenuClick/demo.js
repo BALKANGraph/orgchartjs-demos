4407d3286592e09ae6e7641db7caf9ed3c981f7a
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-02-06 17:00:45 +0200

    auto

---
+++ GetDataOnNodeMenuClick/demo.js
@@ -0,0 +1,29 @@
+
+window.onload = function () { 
+    var chart = new OrgChart(document.getElementById("tree"), {
+        nodeMenu: {
+            add: { text: "Add New" },
+            remove: { text: "Remove" },
+        },
+        nodeBinding: {
+            field_0: "name",
+            field_1: "title",
+            img_0: "img"
+        }
+    });
+    
+       chart.nodeMenuUI.on('show', function(sender, args){
+       		alert(sender.obj.getBGNode(args.firstNodeId).tags);
+       	
+    });
+    
+    chart.load([
+            { id: 1, tags: ["F"], name: "Denny Curtis", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
+            { id: 2, pid: 1,  tags: ["M"], name: "Ashley Barnett", title: "Sales Manager", img: "https://cdn.balkan.app/shared/3.jpg" },
+            { id: 3, pid: 1, tags: ["M"],name: "Caden Ellison", title: "Dev Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
+            { id: 4, pid: 2, tags: ["R"],name: "Elliot Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg" },
+            { id: 5, pid: 2, tags: ["T"],name: "Lynn Hussain", title: "Sales", img: "https://cdn.balkan.app/shared/6.jpg" },
+            { id: 6, pid: 3, tags: ["Y"],name: "Tanner May", title: "Developer", img: "https://cdn.balkan.app/shared/7.jpg" },
+            { id: 7, pid: 3, tags: ["U"],name: "Fran Parsons", title: "Developer", img: "https://cdn.balkan.app/shared/8.jpg" }
+        ]);
+};
