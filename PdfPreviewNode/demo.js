655cabb0e5a42aa9008995125f14c710f0b3e420
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-02-14 09:45:28 +0200

    auto

--- PdfPreviewNode/demo.js
+++ PdfPreviewNode/demo.js
@@ -17,7 +17,11 @@ window.onload = function () {
         nodes: [
             { id: "1", name: "Jack Hill", title: "Chairman and CEO" },
             { id: "2", pid: "1", name: "Lexie Cole", title: "QA Lead" },
-            { id: "3", pid: "1", name: "Janae Barrett", title: "Technical Director"}
+            { id: "3", pid: "1", name: "Janae Barrett", title: "Dev lead"},
+            { id: "4", pid: "2", name: "Elliot Patel", title: "QA"},
+            { id: "5", pid: "2", name: "Lynn Hussain", title: "QA"},
+            { id: "6", pid: "3", name: "Tanner May", title: "Dev"},
+            { id: "7", pid: "3", name: "Fran Parsons", title: "Dev"},
 
         ]
     });
