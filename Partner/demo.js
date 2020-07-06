a700fb57f2c2f65eef8e31e86eabf1b2c146b146
ZornitsaSerbezova <pesheva@gmail.com>
2020-07-06 15:09:17 +0300

    auto

--- Partner/demo.js
+++ Partner/demo.js
@@ -1,40 +1,32 @@
 
 
-var chart;
 window.onload = function () {
-    chart = new OrgChart(document.getElementById("tree"), {
-        nodeMenu: {
-            pdfWithTitle: {                
-                text: 'A4 PDF',
-                icon: OrgChart.icon.pdf(24, 24),
-                onClick: function (id) {
-                                              
-                    chart.exportPDF({
-                        openInNewTab: true,
-                        nodeId: id,
-                        header: 'my header goes here',
-                        footer: 'my footer goes here',
-                        format: 'A4',
-                        margin: [60, 20, 60, 20]
-                    });
-                }
+    var chart = new OrgChart(document.getElementById("tree"), {
+        enableDragDrop: true,
+        tags: {
+            "partner": {
+                template: "ula"
             }
-          },
-
+        },
+        nodeMenu: {
+            details: { text: "Details" },
+            edit: { text: "Edit" },
+            add: { text: "Add" },
+            remove: { text: "Remove" }
+        },
         nodeBinding: {
             field_0: "name",
-            field_1: "title"
+            field_1: "title",
+            img_0: "img"
         },
         nodes: [
-            { id: "1", name: "Jack Hill", title: "Chairman and CEO" },
-            { id: "2", pid: "1", name: "Lexie Cole", title: "QA Lead" },
-            { id: "3", pid: "1", name: "Janae Barrett", title: "Dev lead"},
-            { id: "4", pid: "2", name: "Elliot Patel", title: "QA"},
-            { id: "5", pid: "2", name: "Lynn Hussain", title: "QA"},
-            { id: "6", pid: "3", name: "Tanner May", title: "Dev"},
-            { id: "7", pid: "3", name: "Fran Parsons", title: "Dev"}
-
+            { id: 1, name: "Denny Curtis", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
+            { id: 2, pid: 1, name: "Ashley Barnett", title: "Sales Manager", img: "https://cdn.balkan.app/shared/3.jpg" },
+            { id: 3, pid: 1, name: "Caden Ellison", title: "Dev Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
+            { id: 4, pid: 2, name: "Elliot Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg" },
+            { id: 5, pid: 2, name: "Lynn Hussain", title: "Sales", img: "https://cdn.balkan.app/shared/6.jpg" },
+            { id: 6, pid: 3, name: "Tanner May", title: "Developer", img: "https://cdn.balkan.app/shared/7.jpg" },
+            { id: 8, pid: 1, tags: ["partner"], name: "Rudy Griffiths", title: "Assistant", img: "https://cdn.balkan.app/shared/9.jpg" },
         ]
     });
-
 };
