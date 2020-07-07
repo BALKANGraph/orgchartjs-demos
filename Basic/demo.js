459b4463d1c410dfe282b5bef6e3f0a37e08b7aa
ZornitsaSerbezova <pesheva@gmail.com>
2020-07-07 14:59:34 +0300

    auto

--- Basic/demo.js
+++ Basic/demo.js
@@ -3,8 +3,12 @@ var chart = new OrgChart(document.getElementById("tree"), {
         field_0: "name"
     },
     nodes: [
-        { id: 1, name: "Amber McKenzie" },
-        { id: 2, pid: 1, name: "Ava Field" },
-        { id: 3, pid: 1, name: "Peter Stevens" }
+        { id: 1, name: "Denny Curtis" },
+        { id: 2, pid: 1, name: "Ashley Barnett" },
+        { id: 3, pid: 1, name: "Caden Ellison" },
+        { id: 4, pid: 2, name: "Elliot Patel" },
+        { id: 5, pid: 2, name: "Lynn Hussain" },
+        { id: 6, pid: 3, name: "Tanner May" },
+        { id: 7, pid: 3, name: "Fran Parsons" }
     ]
 });
