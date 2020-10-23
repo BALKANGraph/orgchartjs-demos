6fcb069887b942cf749b2bc38c4f7be7e3e1cbfb
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-10-23 13:27:14 +0300

    auto

--- ExpanCollapseToLevel/demo.js
+++ ExpanCollapseToLevel/demo.js
@@ -23,9 +23,43 @@ window.onload = function () {
             { id: 8, pid: 4},
         ]);
     
-    document.getElementById("levelCount").addEventListener("change", function(e) {          
-      chart.expandCollapseToLevel(1, {
-            level: e.target.value
-        });
-    });
+        function getLevel(node){
+            var pnodeId = node.pid;
+            var nodeLevel = 0;
+            var currentNode = node;
+            while(pnodeId){
+                nodeLevel++;
+                var currentNode = chart.get(pnodeId);
+                pnodeId = currentNode.pid;
+            }
+            return nodeLevel;
+        }
+    
+        document.getElementById("levelCount").addEventListener("change", function(e) {         
+    
+            var id = "1";
+            var level = e.target.value;
+            var expandIds = [];
+            var collapseIds = [];
+            console.log(chart.nodes);
+            var nodeLevel = -1;
+            for (var i in chart.nodes) {
+                if (!chart.nodes[i].level){
+                    nodeLevel = getLevel(chart.nodes[i]);
+                    console.log("nodeLevel " + nodeLevel);
+                }
+                else {
+                    nodeLevel = chart.nodes[i].level;
+                }
+                if (nodeLevel == level){
+                    collapseIds.push(chart.nodes[i].id);
+                }
+                else if (nodeLevel < level){
+                    expandIds.push(chart.nodes[i].id);
+                }
+            }
+    
+            chart.expandCollapse(id, expandIds, collapseIds);
+    
+            });
 };
