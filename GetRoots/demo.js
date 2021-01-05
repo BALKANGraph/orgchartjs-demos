cb4adba59c85e04ded0404200038eea65974cc80
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-01-05 13:34:01 +0200

    auto

---
+++ GetRoots/demo.js
@@ -0,0 +1,36 @@
+   
+window.onload = function () {
+    var chart = new OrgChart(document.getElementById("tree"), {
+        template: "luba",
+      
+        layout: OrgChart.tree,
+        nodeBinding:{
+            field_0: 'id',
+            field_1: 'pid'
+        }
+    });
+    
+       var nodes =  [
+            { id: 2 },
+            { id: 3 },
+            { id: 4, pid: 2 },
+            { id: 5, pid: 2 },
+            { id: 10, pid: 3 },
+            { id: 11, pid: 3 },
+            { id: 12, pid: 3 },
+            { id: 13, pid: 3 },
+            { id: 14, pid: 2 },
+            { id: 15, pid: 2 },
+            { id: 16, pid: 2 }
+        ];
+        
+         
+         chart.on('init', function (sender) {
+            console.log(chart.roots);
+        });  
+
+        
+        chart.load(nodes);
+    
+    
+};
