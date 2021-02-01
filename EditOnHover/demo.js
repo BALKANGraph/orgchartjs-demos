cbad0a6ab439eb533f681a7e80523da2b381ba43
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-02-01 11:14:06 +0200

    auto

--- EditOnHover/demo.js
+++ EditOnHover/demo.js
@@ -6,7 +6,7 @@ window.onload = function () {
         });
 
         chart.on('redraw', function(){
-	        var nodeElements = document.querySelectorAll('[node-id]');
+	          var nodeElements = document.querySelectorAll('[node-id]');
             for(var i = 0; i < nodeElements.length; i++){
   	            nodeElements[i].addEventListener('mouseover', function(e){
                     e.preventDefault();
@@ -26,12 +26,13 @@ window.onload = function () {
             }
         });
 
-chart.load([
-  { id: 1, name: "Denny Curtis" },
-  { id: 2, pid: 1, name: "Ashley Barnett" },
-  { id: 3, pid: 1, name: "Caden Ellison" },
-  { id: 4, pid: 2, name: "Elliot Patel" },
-  { id: 5, pid: 2, name: "Lynn Hussain" },
-  { id: 6, pid: 3, name: "Tanner May" },
-  { id: 7, pid: 3, name: "Fran Parsons" }
-]);
+  chart.load([
+    { id: 1, name: "Denny Curtis" },
+    { id: 2, pid: 1, name: "Ashley Barnett" },
+    { id: 3, pid: 1, name: "Caden Ellison" },
+    { id: 4, pid: 2, name: "Elliot Patel" },
+    { id: 5, pid: 2, name: "Lynn Hussain" },
+    { id: 6, pid: 3, name: "Tanner May" },
+    { id: 7, pid: 3, name: "Fran Parsons" }
+  ]);
+}
