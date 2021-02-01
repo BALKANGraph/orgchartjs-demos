86a55d17edb444e799b7c9794d4ff572473e3087
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-02-01 11:05:18 +0200

    auto

--- EditOnHover/demo.js
+++ EditOnHover/demo.js
@@ -1,32 +1,37 @@
-var chart = new OrgChart(document.getElementById("tree"), {
-    nodeBinding: {
-        field_0: "name"
-    }
-});
+window.onload = function () {
+  var chart = new OrgChart(document.getElementById("tree"), {
+            nodeBinding: {
+                field_0: "name"
+            }
+        });
 
-chart.on('redraw', function(){
-	var nodeElements = document.querySelectorAll('[node-id]');
-  for(var i = 0; i < nodeElements.length; i++){
-  	nodeElements[i].addEventListener('mouseover', function(){
-    	var nodeId = this.getAttribute('node-id');
-      chart.editUI.show(nodeId, true);
-    })
-  }
-  
-    for(var i = 0; i < nodeElements.length; i++){
-  	nodeElements[i].addEventListener('mouseleave', function(){
-			var nodeId = this.getAttribute('node-id');
-      chart.editUI.hide(nodeId);
-    })
-  }
-});
+        chart.on('redraw', function(){
+	        var nodeElements = document.querySelectorAll('[node-id]');
+            for(var i = 0; i < nodeElements.length; i++){
+  	            nodeElements[i].addEventListener('mouseover', function(e){
+                    e.preventDefault();
+                    
+                    if (e.target.tagName != 'text'){
+                        var nodeId = this.getAttribute('node-id');
+                        chart.editUI.show(nodeId, true);                
+                    }
+
+                });
+
+            
+                nodeElements[i].addEventListener('mouseleave', function(e){
+                    e.preventDefault();
+                    document.querySelector('.edit-wrapper').style.display = 'none';
+                });
+            }
+        });
 
 chart.load([
-        { id: 1, name: "Denny Curtis" },
-        { id: 2, pid: 1, name: "Ashley Barnett" },
-        { id: 3, pid: 1, name: "Caden Ellison" },
-        { id: 4, pid: 2, name: "Elliot Patel" },
-        { id: 5, pid: 2, name: "Lynn Hussain" },
-        { id: 6, pid: 3, name: "Tanner May" },
-        { id: 7, pid: 3, name: "Fran Parsons" }
-    ]);
+  { id: 1, name: "Denny Curtis" },
+  { id: 2, pid: 1, name: "Ashley Barnett" },
+  { id: 3, pid: 1, name: "Caden Ellison" },
+  { id: 4, pid: 2, name: "Elliot Patel" },
+  { id: 5, pid: 2, name: "Lynn Hussain" },
+  { id: 6, pid: 3, name: "Tanner May" },
+  { id: 7, pid: 3, name: "Fran Parsons" }
+]);
