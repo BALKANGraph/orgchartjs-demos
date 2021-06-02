41d5ea863a1d9c077f310e3388bde97280c3b0e6
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-06-02 20:40:05 +0300

    auto

--- EditCustomField/demo.js
+++ EditCustomField/demo.js
@@ -1,27 +1,32 @@
 window.onload = function () { 
-    var chart = new OrgChart(document.getElementById("tree"), {
-        enableSearch: false,
-        nodeBinding: {
-            field_0: function(sender, node){return 'level: ' + node.level},
-        },
-    });
-    
-    chart.editUI.on('field', function(sender, args){
-      if (String(args.name).includes('function(sender, node)')){
+  var chart = new OrgChart(document.getElementById("tree"), {
+      enableSearch: false,
+      nodeBinding: {
+          field_0: null,
+      },
+  });
 
-        var lbl = args.field.querySelector('div');
-        lbl.innerHTML = "Level:";
-        var txt = args.field.querySelector('input');
-        txt.value = chart.getNode(sender.node.id).level;   
+  chart.on('field', function(sender, args){
+      if (args.name == null){
+          args.value = "level: " + chart.getNode(args.data.id).level;   
       }
-    });
+  });
+  
+  chart.editUI.on('field', function(sender, args){
+      if (args.name == null){
+          var lbl = args.field.querySelector('div');
+          lbl.innerHTML = "Level:";
+          var txt = args.field.querySelector('input');
+          txt.value = chart.getNode(sender.node.id).level;   
+    }
+  });
 
-    
-    chart.load([
-            { id: 1},
-            { id: 2, pid: 1},
-            { id: 3, pid: 1},
-    ]);
-        
+  
+  chart.load([
+          { id: 1},
+          { id: 2, pid: 1},
+          { id: 3, pid: 1},
+  ]);
+      
 
 };
