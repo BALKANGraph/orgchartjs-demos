9de36433fe020fad50e65bb35f095bbabba12689
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-06-03 17:24:14 +0300

    auto

--- PopupOnFieldClick/demo.js
+++ PopupOnFieldClick/demo.js
@@ -7,19 +7,20 @@ window.onload = function () {
   }
 
 OrgChart.templates.ana.field_1 = '{val}';
+
     var chart = new OrgChart(document.getElementById("tree"), {
         enableDragDrop: true,
         nodeMouseClick: OrgChart.action.none,
         nodeBinding: {
-        		field_0: "name",
-            field_1: function(sender, node){
-            var data = sender.get(node.id)
-            return '<a onclick="showPopup()" href="#"><text width="130" text-overflow="multiline" class="field_1"  style="font-size: 14px;" fill="#ffffff" x="200" y="30" text-anchor="middle">{val}</text></a>'.replace('{val}', data.title)
-            },
-            
-            img_0: "img"
+        	field_0: "name",
+          field_1: "title",
+          img_0: "img"
         },
-        nodes: [
+
+    });
+
+
+    var nodes = [
             { id: 1, name: "Denny Curtis", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
             { id: 2, pid: 1, name: "Ashley Barnett", title: "Sales Manager", img: "https://cdn.balkan.app/shared/3.jpg" },
             { id: 3, pid: 1, name: "Caden Ellison", title: "Dev Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
@@ -27,6 +28,15 @@ OrgChart.templates.ana.field_1 = '{val}';
             { id: 5, pid: 2, name: "Lynn Hussain", title: "Sales", img: "https://cdn.balkan.app/shared/6.jpg" },
             { id: 6, pid: 3, name: "Tanner May", title: "Developer", img: "https://cdn.balkan.app/shared/7.jpg" },
             { id: 7, pid: 3, name: "Fran Parsons", title: "Developer", img: "https://cdn.balkan.app/shared/8.jpg" }
-        ]
-    });
+        ];
+
+
+    chart.on('field', function(sender, args){
+        if (args.name == "title"){
+            args.value = '<a onclick="showPopup()" href="#"><text width="130" text-overflow="multiline" class="field_1" style="font-size: 14px;" fill="#ffffff" x="200" y="30" text-anchor="middle">{val}</text></a>'.replace('{val}', args.data.title)
+        }
+     });
+
+    chart.load(nodes);
+
 };
