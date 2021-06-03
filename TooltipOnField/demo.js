bb65e3dde594b2b771f3ee54ad13a1008135d329
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-06-03 17:51:09 +0300

    auto

--- TooltipOnField/demo.js
+++ TooltipOnField/demo.js
@@ -2,12 +2,16 @@ OrgChart.templates.ana.field_0 = '{val}';
 
 var chart = new OrgChart(document.getElementById("tree"), {
     nodeBinding: {
-        field_0: function(sender, node){
-           var data = sender.get(node.id);
-            return '<text width="230" style="font-size: 18px;" fill="#ffffff" x="{cw}" y="30" text-anchor="middle" >' + data.title + ' <title>' + data.titleTooltip + '</title> </text>'
-        }
+        field_0: "title"
     },
 
 });
 
+chart.on('field', function(sender, args){
+        if (args.name == "title"){
+            args.value = '<text width="230" style="font-size: 18px;" fill="#ffffff" x="{cw}" y="30" text-anchor="middle" >' + args.data.title + ' <title>' + args.data.titleTooltip + '</title> </text>';
+        }
+     });
+
 chart.load([{id: 0, title: 'My Title', titleTooltip: 'This is the tooltip for My Title'}])
+ 
