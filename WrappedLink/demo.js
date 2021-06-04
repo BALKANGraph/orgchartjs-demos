1d594d0f20f02971145a40aaef383a6b4da0d28a
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-06-04 15:51:16 +0300

    auto

--- WrappedLink/demo.js
+++ WrappedLink/demo.js
@@ -1,23 +1,30 @@
 var field_template = '<text width="230" text-overflow="ellipsis"  style="font-size: 24px;" fill="#757575" x="125" y="100" text-anchor="middle">{val}</text>';						
- OrgChart.templates.olivia.field_0 = '{val}';
- 
-            var chart = new OrgChart(document.getElementById("tree"), {
-             		mouseScrool: OrgChart.action.none,
-                template: "luba",
-                nodeMouseClick: OrgChart.action.none,
-                nodeBinding: {
-                    field_0: function (sender, node) {
-                        var data = sender.get(node.id);
-                        var name = data["name"];
-                        var link = data["link"];                     
-                        var text =  OrgChart.wrapText(name, field_template);
-                        var fieldData = '<a target="_blank" href="' + link + '">' + text + '</a>';
-                        return fieldData;
-                    }
-                },
-                nodes: [
-                    { id: 1, name: "some longer link name", link: "https://balkangraph.com" },
-                    { id: 2, pid: 1, name: "another longer link name", link: "https://webcall.me"  },
-                    { id: 3, pid: 1, name: "GetOrgChart", link: "http://getorgchart.com" }
-                ]
-            });
+OrgChart.templates.olivia.field_0 = '{val}';
+
+           var chart = new OrgChart(document.getElementById("tree"), {
+                mouseScrool: OrgChart.action.none,
+               template: "luba",
+               nodeMouseClick: OrgChart.action.none,
+               nodeBinding: {
+                   field_0: "name"
+               },
+               
+           });
+
+           var nodes = [
+                   { id: 1, name: "BALKAN OrgChart JS", link: "https://balkan.app/OrgChartJS" },
+                   { id: 2, pid: 1, name: "BALKAN App on linkedIn", link: "https://www.linkedin.com/company/balkanapp"  },
+                   { id: 3, pid: 1, name: "BALKAN OrgChart", link: "https://balkan.app/OrgChart" }
+               ];
+
+   chart.on('field', function(sender, args){
+       if (args.name == "name"){
+           var name = args.data["name"];
+           var link = args.data["link"];                     
+           var text =  OrgChart.wrapText(name, field_template);
+           var fieldData = '<a target="_blank" href="' + link + '">' + text + '</a>';
+           args.value = fieldData;
+       }
+    });
+
+           chart.load(nodes);
