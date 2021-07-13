fe2a1da64b51e3cfe70e1d2a9b89250121e3bef0
ZornitsaPesheva <pesheva@gmail.com>
2021-07-13 11:37:28 +0300

    auto

--- TooltipMutilineOnMouseover/demo.js
+++ TooltipMutilineOnMouseover/demo.js
@@ -3,6 +3,7 @@ window.onload = function () {
     var chart = new OrgChart(document.getElementById('tree'), {
         template: 'ana',
         nodeBinding: {
+            field_0: "name",
             tooltip: 'tooltip'
         }
     });
@@ -33,16 +34,46 @@ window.onload = function () {
             });
         }
     });
+
+    // tooltip on a field
+    // chart.on('redraw', function (sender) {
+    //     var nodeElements = sender.element.querySelectorAll('[node-id]');
+    //     var fieldElements = sender.element.querySelectorAll('[node-id] text');
+    //     for (var i = 0; i < fieldElements.length; i++) {
+    //        var nodeElement = nodeElements[i];
+    //         var fieldElement = fieldElements[i];
+    //        fieldElement.addEventListener('mouseover', function (e) {
+    //             var id = nodeElement.getAttribute('node-id');
+    //             var tooltip = document.getElementById("tooltip");
+    //             var tooltipContent = document.querySelector(".tooltip-content");
+               
+    //             var position = OrgChart._menuPosition(this, tooltip, sender.getSvg());
+                
+    //             tooltipContent.innerHTML = sender.get(id).tooltip;
+    //             tooltip.style.left = position.x + 50 + "px";
+    //             tooltip.style.top =  position.y + "px";
+    //             tooltip.classList.add("visible");
+    
+    //         });
+        
+    //         fieldElement.addEventListener('mouseleave', function () {
+    //             var tooltip = document.getElementById("tooltip");
+    //             if (tooltip){
+    //                 tooltip.classList.remove('visible');
+    //             }
+    //         });
+    //     }
+    // });
     
     
     chart.load([
-        { id: 0, tooltip: 'At vero eos et accusamus et iusto odio'  },
-        { id: 1, pid: 0, tooltip: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
-        { id: 2, pid: 0, tooltip: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium' },
-        { id: 3, pid: 1, tooltip: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti' },
-        { id: 4, pid: 1, tooltip: 'Lorem ipsum dolor sit amet' },
-        { id: 5, pid: 2, tooltip: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem' },
-        { id: 6, pid: 2, tooltip: 'At vero eos et accusamus et iusto odio dignissimos' }
+        { id: 0, name: "node0", tooltip: 'At vero eos et accusamus et iusto odio'  },
+        { id: 1, name: "node1", pid: 0, tooltip: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
+        { id: 2, name: "node2", pid: 0, tooltip: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium' },
+        { id: 3, name: "node3", pid: 1, tooltip: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti' },
+        { id: 4, name: "node4", pid: 1, tooltip: 'Lorem ipsum dolor sit amet' },
+        { id: 5, name: "node5", pid: 2, tooltip: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem' },
+        { id: 6, name: "node6", pid: 2, tooltip: 'At vero eos et accusamus et iusto odio dignissimos' }
     ]);
     
     
