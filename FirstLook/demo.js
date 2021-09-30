45708782a4234f81f556a67709554a8fca1ed533
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-09-30 11:32:23 +0300

    auto

--- FirstLook/demo.js
+++ FirstLook/demo.js
@@ -1,8 +1,16 @@
-
-
 window.onload = function () {
     OrgChart.templates.ana.plus = '<circle cx="15" cy="15" r="15" fill="#ffffff" stroke="#aeaeae" stroke-width="1"></circle>'
         + '<text text-anchor="middle" style="font-size: 18px;cursor:pointer;" fill="#757575" x="15" y="22">{collapsed-children-count}</text>';
+
+    OrgChart.templates.itTemplate = Object.assign({}, OrgChart.templates.ana);
+    OrgChart.templates.itTemplate.nodeMenuButton = "";
+    OrgChart.templates.itTemplate.nodeCircleMenuButton = {
+                radius: 18,
+                x: 250,
+                y: 60,
+                color: '#fff',
+                stroke: '#aeaeae'
+            };
 
     OrgChart.templates.invisibleGroup.padding = [20, 0, 0, 0];
 
@@ -10,6 +18,34 @@ window.onload = function () {
         template: "ana",
         enableDragDrop: true,
         assistantSeparation: 170,
+        nodeCircleMenu: {
+            details: {
+              icon: OrgChart.icon.details(24, 24, '#aeaeae'),
+              text: "Details",
+              color: "white"
+            },
+            edit: {
+              icon: OrgChart.icon.edit(24, 24, '#aeaeae'),
+              text: "Edit node",
+              color: "white"
+            },
+            add: {
+              icon: OrgChart.icon.add(24, 24, '#aeaeae'),
+              text: "Add node",
+              color: "white"
+            },
+            remove: {
+              icon: OrgChart.icon.remove(24, 24, '#aeaeae'), 
+              text: "Remove node",
+              color: '#fff',
+            },
+            addLink: {
+              icon: OrgChart.icon.link(24, 24, '#aeaeae'), 
+              text: "Add C link(drag and drop)",
+              color: '#fff',
+              draggable: true
+            }
+          },
         menu: {
             pdfPreview: {
                 text: "Export to PDF",
@@ -94,8 +130,32 @@ window.onload = function () {
                     edit: { text: "Edit department" },
                     nodePdfPreview: { text: "Export department to PDF", icon: OrgChart.icon.pdf(24, 24, "#7A7A7A"), onClick: nodePdfPreview }
                 }
+            },
+            "it-team-member": {
+              template: "itTemplate",
             }
         }
+    });
+
+    chart.nodeCircleMenuUI.on('click', function (sender, args) {
+          switch (args.menuItem.text) {
+              case "Details": chart.editUI.show(args.nodeId, true);
+              break;
+              case "Add node": {
+                  var id = chart.generateId();
+                  chart.addNode({ id: id, pid: args.nodeId, tags: ["it-team-member"]});
+              }
+              break;
+              case "Edit node": chart.editUI.show(args.nodeId);
+              break;
+              case "Remove node": chart.removeNode(args.nodeId);
+              break;
+              default:
+          };
+      });
+
+    chart.nodeCircleMenuUI.on('drop', function (sender, args) {
+            chart.addClink(args.from, args.to).draw(OrgChart.action.update);        
     });
 
     chart.on("added", function (sender, id) {
@@ -142,15 +202,16 @@ window.onload = function () {
         { id: 5, pid: 4, name: "Emerson Adams", title: "Senior HR", img: "https://cdn.balkan.app/shared/5.jpg" },
         { id: 6, pid: 4, name: "Kai Morgan", title: "Junior HR", img: "https://cdn.balkan.app/shared/6.jpg" },
 
-        { id: 7, stpid: "it-team", name: "Cory Robbins", title: "Core Team Lead", img: "https://cdn.balkan.app/shared/7.jpg" },
-        { id: 8, pid: 7, name: "Billie Roach", title: "Backend Senior Developer", img: "https://cdn.balkan.app/shared/8.jpg" },
-        { id: 9, pid: 7, name: "Maddox Hood", title: "C# Developer", img: "https://cdn.balkan.app/shared/9.jpg" },
-        { id: 10, pid: 7, name: "Sam Tyson", title: "Backend Junior Developer", img: "https://cdn.balkan.app/shared/10.jpg" },
+        { id: 7, stpid: "it-team", name: "Cory Robbins", tags: ["it-team-member"], title: "Core Team Lead", img: "https://cdn.balkan.app/shared/7.jpg" },
+        { id: 8, pid: 7, name: "Billie Roach", tags: ["it-team-member"], title: "Backend Senior Developer", img: "https://cdn.balkan.app/shared/8.jpg" },
+        { id: 9, pid: 7, name: "Maddox Hood", tags: ["it-team-member"], title: "C# Developer", img: "https://cdn.balkan.app/shared/9.jpg" },
+        { id: 10, pid: 7, name: "Sam Tyson", tags: ["it-team-member"], title: "Backend Junior Developer", img: "https://cdn.balkan.app/shared/10.jpg" },
 
-        { id: 11, stpid: "it-team", name: "Lynn Fleming", title: "UI Team Lead", img: "https://cdn.balkan.app/shared/11.jpg" },
-        { id: 12, pid: 11, name: "Jo Baker", title: "JS Developer", img: "https://cdn.balkan.app/shared/12.jpg" },
-        { id: 13, pid: 11, name: "Emerson Lewis", title: "Graphic Designer", img: "https://cdn.balkan.app/shared/13.jpg" },
-        { id: 14, pid: 11, name: "Haiden Atkinson", title: "UX Expert", img: "https://cdn.balkan.app/shared/14.jpg" },
+        { id: 11, stpid: "it-team", name: "Lynn Fleming", tags: ["it-team-member"], title: "UI Team Lead", img: "https://cdn.balkan.app/shared/11.jpg" },
+        { id: 12, pid: 11, name: "Jo Baker", tags: ["it-team-member"], title: "JS Developer", img: "https://cdn.balkan.app/shared/12.jpg" },
+        { id: 13, pid: 11, name: "Emerson Lewis", tags: ["it-team-member"], title: "Graphic Designer", img: "https://cdn.balkan.app/shared/13.jpg" },
+        { id: 14, pid: 11, name: "Haiden Atkinson", tags: ["it-team-member"], title: "UX Expert", img: "https://cdn.balkan.app/shared/14.jpg" },
+    
         { id: 15, stpid: "sales-team", name: "Tyler Chavez", title: "Sales Manager", img: "https://cdn.balkan.app/shared/15.jpg" },
         { id: 16, pid: 15, name: "Raylee Allen", title: "Sales", img: "https://cdn.balkan.app/shared/16.jpg" },
         { id: 17, pid: 15, name: "Kris Horne", title: "Sales Guru", img: "https://cdn.balkan.app/shared/8.jpg" },
