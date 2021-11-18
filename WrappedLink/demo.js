aab0477de27659896781f8bdee5dd0cb469d4444
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2021-11-18 14:09:29 +0200

    auto

--- WrappedLink/demo.js
+++ WrappedLink/demo.js
@@ -1,5 +1,4 @@
-var field_template = '<text data-width="230" data-text-overflow="ellipsis"  style="font-size: 24px;" fill="#757575" x="125" y="100" text-anchor="middle">{val}</text>';
-OrgChart.templates.olivia.field_0 = '{val}';
+var field_template = '<text data-width="190" data-text-overflow="ellipsis"  style="font-size: 24px;" fill="#757575" x="125" y="100" text-anchor="middle">{val}</text>';
 
 var chart = new OrgChart(document.getElementById("tree"), {
     mouseScrool: OrgChart.action.none,
